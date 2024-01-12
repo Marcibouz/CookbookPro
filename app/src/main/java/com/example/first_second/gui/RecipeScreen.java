9a1c08c1e84e7251edaa589fc6c2bfed6206bd1c
Jaden Diodone <jaden@diodone.de>
2024-01-12 17:02:42 +0100

    Refactoring, Added BluetoothHelper Interface

--- app/src/main/java/com/example/first_second/gui/RecipeScreen.java
+++ app/src/main/java/com/example/first_second/gui/RecipeScreen.java
@@ -23,6 +23,7 @@ import com.example.first_second.R;
 import com.example.first_second.application_logic.ApplicationLogic;
 import com.example.first_second.application_logic.ApplicationLogicImpl;
 import com.example.first_second.bluetooth.BluetoothHelper;
+import com.example.first_second.bluetooth.BluetoothHelperImpl;
 import com.example.first_second.databinding.RecipescreenBinding;
 import com.example.first_second.local_memory.LocalMemoryImpl;
 import com.example.first_second.local_memory.LocalMemory;
@@ -164,7 +165,7 @@ public class RecipeScreen extends Fragment {
         @RequiresApi(api = Build.VERSION_CODES.S)
         @Override
         public void onClick(View view) {
-            BluetoothHelper bluetoothHelper = new BluetoothHelper(context, activity);
+            BluetoothHelper bluetoothHelper = new BluetoothHelperImpl(context, activity);
             if (bluetoothHelper.checkPermissions()) { // Permission check
                 if (bluetoothHelper.activateBluetooth()) {
                     RecipeScreenDirections.RecipeScreenToAvailableDevicesScreen action =
