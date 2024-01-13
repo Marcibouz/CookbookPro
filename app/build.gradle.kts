ac9d82a638179a11477514f1305e9a77810d521a
Marcibouz <marcio_holz@gmx.de>
2024-01-14 00:03:05 +0100

    gradle file espresso und gui test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("org.mockito:mockito-core:3.12.4")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }
---
+++ app/src/androidTest/java/com/example/first_second/GuiTest.java
@@ -0,0 +1,53 @@
+package com.example.first_second;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
+
+import com.example.first_second.gui.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class GuiTest {
+
+    @Rule
+    public GrantPermissionRule bluetoothPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH);
+    @Rule
+    public GrantPermissionRule bluetoothPermissionRule1 = GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH_ADMIN);
+    @Rule
+    public GrantPermissionRule bluetoothPermissionRule2 = GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH_ADVERTISE);
+    @Rule
+    public GrantPermissionRule bluetoothPermissionRule3 = GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH_CONNECT);
+    @Rule
+    public GrantPermissionRule bluetoothPermissionRule4 = GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH_SCAN);
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testSplashScreenToRecipeListScreen() {
+        onView(withId(R.id.splashScreen)).perform(click());
+        onView(withId(R.id.recipeListScreen)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testAddButton() {
+        onView(withId(R.id.splashScreen)).perform(click());
+//        onView(withId(R.id.recipeListScreen)).check(matches(isDisplayed()));
+        onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.recipeScreen)).check(matches(isDisplayed()));
+    }
+}
