0b14716cca7433a0e5246519d278787715156351
Marcibouz <marcio_holz@gmx.de>
2024-01-14 00:52:39 +0100

    addscreen id gegeben und GUI test fertig

--- app/src/androidTest/java/com/example/first_second/GuiTest.java
+++ app/src/androidTest/java/com/example/first_second/GuiTest.java
@@ -2,6 +2,8 @@ package com.example.first_second;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -22,7 +24,6 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class GuiTest {
-
     @Rule
     public GrantPermissionRule bluetoothPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH);
     @Rule
@@ -36,7 +37,6 @@ public class GuiTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
-
     @Test
     public void testSplashScreenToRecipeListScreen() {
         onView(withId(R.id.splashScreen)).perform(click());
@@ -46,8 +46,57 @@ public class GuiTest {
     @Test
     public void testAddButton() {
         onView(withId(R.id.splashScreen)).perform(click());
-//        onView(withId(R.id.recipeListScreen)).check(matches(isDisplayed()));
         onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.addScreen)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSaveButton() {
+        onView(withId(R.id.splashScreen)).perform(click());
+        onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withId(R.id.recipeListScreen)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testRowElementClick() {
+        onView(withId(R.id.splashScreen)).perform(click());
+        onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.recipe_name)).perform(typeText("A"), closeSoftKeyboard());
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withText("A")).perform(click());
         onView(withId(R.id.recipeScreen)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testUpdateButton() {
+        onView(withId(R.id.splashScreen)).perform(click());
+        onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.recipe_name)).perform(typeText("B"), closeSoftKeyboard());
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withText("B")).perform(click());
+        onView(withId(R.id.update_button)).perform(click());
+        onView(withId(R.id.recipeListScreen)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testShareButton() {
+        onView(withId(R.id.splashScreen)).perform(click());
+        onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.recipe_name)).perform(typeText("C"), closeSoftKeyboard());
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withText("C")).perform(click());
+        onView(withId(R.id.share_button)).perform(click());
+        onView(withId(R.id.availableDevicesScreen)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void testDeleteButton() {
+        onView(withId(R.id.splashScreen)).perform(click());
+        onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.recipe_name)).perform(typeText("D"), closeSoftKeyboard());
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withText("D")).perform(click());
+        onView(withId(R.id.delete_button)).perform(click());
+        onView(withId(R.id.recipeListScreen)).check(matches(isDisplayed()));
     }
 }
